a0cc7ea80bf15aafd2b2dd1030e3100d797f5a8b
priyanshuPC <vishnuc53236@gmail.com>
2024-07-02 22:21:23 +0530

    blog web

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <img src="https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_640.png" alt="Logo"  className='w-12 rounded-full'/>
+    </div>
+  )
+}
+
+export default Logo
